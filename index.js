075bd670b354178f1d24c9c458307ace4ef356a1
Kelly Rogers <kellyerogers92@gmail.com>
2023-11-14 17:05:51 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+let names = ["Guadalupe", "Ollie", "Aki"]
+let event = "surprise"
+
+function writeCards(names, event){
+    let messages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+        messages.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return messages;
+}
+
+//
+
+let number = 10
+
+function countDown (number) {
+ while (number >= 0) {
+    console.log(number--)
+ }
+}
+
